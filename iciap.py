354fe5a3c51e449fa08e0005694bf8a7818783e5
vanhao-le <haole.hdu@gmail.com>
2023-04-18 09:27:16 +0200

    update iciap

--- iciap.py
+++ iciap.py
@@ -1,23 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import matplotlib.font_manager as font_manager
 
 
 def cnn_plot():
-    ZNCC_RESULT = r"output_setD\zncc_result.npz"
-    VGG_RESULT = r"output_setD\vgg16_result.npz"
-    RES_RESULT = r"output_setD\rs50_result.npz"
-    GGL_RESULT = r"output_setD\ggl_result.npz"
-
-    KF_RESULT = r"output\KF_result.npz"
-
-    # zncc_data = np.load(ZNCC_RESULT)
-    # zncc_threshold = zncc_data['threshold']
-    # zncc_precision = zncc_data['precision']
-    # zncc_recall = zncc_data['recall']
-    # zncc_f1 = zncc_data['F1_score']
-
+    
+    font_path = r"C:\Windows\Fonts\times.ttf"
+    font_prop = font_manager.FontProperties(fname=font_path, size=14)
+    font_prop_lable = font_manager.FontProperties(fname=font_path, size=16)
+    font_prop_legend = font_manager.FontProperties(fname=font_path, size=13)
+    
+    VGG_RESULT = r"iciap_data\vgg16_result.npz"
+    RES_RESULT = r"iciap_data\rs50_result.npz"
+    GGL_RESULT = r"iciap_data\ggl_result.npz"
+    MAC_RS50 = r"iciap_data\MAC_rs50_result.npz"
+    MAC_VGG = r"iciap_data\MAC_vgg16_result.npz"
+    RMAC_RS50 = r"iciap_data\RMAC_rs50_result.npz"
+    RMAC_VGG = r"iciap_data\RMAC_vgg16_result.npz"
 
     vgg_data = np.load(VGG_RESULT)
     vgg_threshold = vgg_data['threshold']
@@ -31,11 +31,6 @@ def cnn_plot():
     rs_recall = rs_data['recall']
     rs_f1 = rs_data['F1_score']
 
-    kf_data = np.load(KF_RESULT)
-    kf_threshold = kf_data['threshold']
-    kf_precision = kf_data['precision']
-    kf_recall = kf_data['recall']
-    kf_f1 = kf_data['F1_score']
 
     ggl_data = np.load(GGL_RESULT)
     ggl_threshold = ggl_data['threshold']
@@ -43,44 +38,77 @@ def cnn_plot():
     ggl_recall = ggl_data['recall']
     ggl_f1 = ggl_data['F1_score']
 
+    mac_rs = np.load(MAC_RS50)
+    mac_rs_threshold = mac_rs['threshold']
+    mac_rs_f1 = mac_rs['F1_score']
+
+    mac_vgg = np.load(MAC_VGG)
+    mac_vgg_threshold = mac_vgg['threshold']
+    mac_vgg_f1 = mac_vgg['F1_score']
+
+
+    rmac_rs = np.load(RMAC_RS50)
+    rmac_rs_threshold = rmac_rs['threshold']
+    rmac_rs_f1 = rmac_rs['F1_score']
+
+    rmac_vgg = np.load(RMAC_VGG)
+    rmac_vgg_threshold = rmac_vgg['threshold']
+    rmac_vgg_f1 = rmac_vgg['F1_score']
 
     # #create precision recall curve
     fig, (ax1, ax2) = plt.subplots(1, 2)
 
-    ax1.plot(kf_recall, kf_precision, color='red', label="ResNet-50 (Keyfram selection)")
-    ax1.plot(rs_recall, rs_precision, color='black', label="ResNet-50 (FPS = 1)")
-    # ax1.plot(rs_recall, rs_precision, color='black', label="ResNet-50 (224x224x3, 2048-D)")
-    # ax1.plot(ggl_recall, ggl_precision, color='cyan', label="GGL-v3 (299x299x3, 2048-D)")
-    # ax1.plot(vgg_recall, vgg_precision, color='red', label="VGG-16 (224x224x3, 4096-D)")
-    # ax1.plot(zncc_recall, zncc_precision, color='blue', label="ZNCC (80x60x1, 4800-D)")
-    # add axis labels to plot
-    ax1.set_title('Precision-Recall Curve')
-    ax1.set_ylabel('Precision')
-    ax1.set_xlabel('Recall')
-    ax1.set_ylim(top=1)
+    
+    ax1.plot(rs_threshold, rs_f1, color = "black", label="ResNet-50-v1 (Last FC)", linestyle='-')
+    ax1.plot(ggl_threshold, ggl_f1, color='black', label="Inception-v3 (Last FC)", linestyle='-.')
+    ax1.plot(vgg_threshold, vgg_f1, color='black', label="VGG-16 (Last FC)", linestyle='--')
+    
+    # add axis labels to plot    
+    ax1.set_ylabel('$F_1$ scores', fontproperties=font_prop_lable)
+    ax1.set_xlabel('Thresholds \n (a)', fontproperties=font_prop_lable)    
+ 
+    ax1.set_xlim(0.75, 1.)
+    ax1.set_ylim(0., 1.0)
 
-    ax1.legend(loc="lower left")
-    ax1.grid()
+    for label in (ax1.get_xticklabels() + ax1.get_yticklabels()):
+        label.set_fontproperties(font_prop)
+    ax1.legend(loc="lower left", prop=font_prop_legend)
+     
+    ax1.grid(which='major', linestyle='--')
 
-    ax2.plot(kf_threshold, kf_f1, color='red', label="ResNet-50 (Keyfram selection)")
-    ax2.plot(rs_threshold, rs_f1, color='black', label="ResNet-50 (FPS = 1)")
+    marker_size= 5
+    markevery = 0.1
 
-    # ax2.plot(rs_threshold, rs_f1, color='black', label="ResNet-50 (224x224x3, 2048-D)")
-    # ax2.plot(ggl_threshold, ggl_f1, color='cyan', label="GGL-v3 (299x299x3, 2048-D)")
-    # ax2.plot(vgg_threshold, vgg_f1, color='red', label="VGG-16 (224x224x3, 4096-D)")
-    # ax2.plot(zncc_threshold, zncc_f1, color='blue', label="ZNCC (80x60x1, 4800-D)")
-    # ax2.set_title('Precision-Recall Curve')
-    ax2.set_ylabel('F1 score')
-    ax2.set_ylim(top=1)
-    ax2.set_xlabel('Threshold')
-    # ax2.set_xlim(left=0, right=1)
-    ax2.legend(loc="lower left")
-    ax2.grid()
+    ax2.plot(rs_threshold, rs_f1, color='black', label="ResNet-50-v1 (Last FC)", linestyle='-', marker='.', ms=marker_size, markevery=markevery)
+    ax2.plot(vgg_threshold, vgg_f1, color='black', label="VGG-16 (Last FC)", linestyle='--', marker='.', ms=marker_size, markevery=markevery)
+
+    ax2.plot(mac_rs_threshold, mac_rs_f1, color='black', label="ResNet-50-v1 (MAC)", linestyle='-', marker='>', ms=marker_size, markevery=markevery)
+    ax2.plot(mac_vgg_threshold, mac_vgg_f1, color='black', label="VGG-16 (MAC)", linestyle='--', marker='>', ms=marker_size, markevery=markevery)
+
+    ax2.plot(rmac_rs_threshold, rmac_rs_f1, color='black', label="ResNet-50-v1 (R-MAC)", linestyle='-', marker='*', ms=marker_size, markevery=markevery)
+    ax2.plot(rmac_vgg_threshold, rmac_vgg_f1, color='black', label="VGG-16 (R-MAC)", linestyle='--', marker='*', ms=marker_size, markevery=markevery)
+    
+    # add axis labels to plot    
+    ax2.set_ylabel('$F_1$ scores', fontproperties=font_prop_lable)
+    ax2.set_xlabel('Thresholds \n (b)', fontproperties=font_prop_lable)    
+ 
+    ax2.set_xlim(0.75, 1.)
+    ax2.set_ylim(0., 1.0)
+
+    for label in (ax2.get_xticklabels() + ax2.get_yticklabels()):
+        label.set_fontproperties(font_prop)
+    ax2.legend(loc="lower left", prop=font_prop_legend)
+     
+    ax2.grid(which='major', linestyle='--')
 
     #display plot
     plt.show()
 
 def criterion_plot():
+    font_path = r"C:\Windows\Fonts\times.ttf"
+    font_prop = font_manager.FontProperties(fname=font_path, size=14)
+    font_prop_lable = font_manager.FontProperties(fname=font_path, size=16)
+    font_prop_legend = font_manager.FontProperties(fname=font_path, size=12)
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
@@ -94,23 +122,27 @@ def criterion_plot():
             data[score] += 1
         else:
             data[score] = 1        
-
     
-
     lists = sorted(data.items())
     x_frame, y_frame = zip(*lists)
     ax1.plot(x_frame, y_frame, color='black', label='$\phi(X)$ distribution')
-    ax1.legend(loc="upper left")
+    line_0 = ax1.vlines(x=0., ymin=0, ymax=1000, color='red', linestyle='--', label=r'$\phi(X)=0$')
+    
+    # ax1.set_ylim(0., 950)
+    ax1.set_xticks([-0.6, -0.4, -0.2, 0., 0.2])
+    ax1.set_yticks([0, 200, 400, 600, 800, 1000])
+    ax1.set_xlabel('$\phi(X)$ scores \n (a)', fontproperties=font_prop_lable)   
+
+    for label in (ax1.get_xticklabels() + ax1.get_yticklabels()):
+        label.set_fontproperties(font_prop)
+    ax1.legend(handles=[line_0 ], loc="upper left", prop=font_prop_legend)
+    ax1.grid(which='major', linestyle='--')
     
     
     REFERENCE_FILE = r'iciap_data\reference_characterization.csv'
-
     df = pd.read_csv(REFERENCE_FILE)
-
     score_list = df['std_score'].to_numpy()
-
     x = np.arange(0., 0.13, 0.01)
-
     data = {}
     for i in x:
         count = 0
@@ -123,40 +155,63 @@ def criterion_plot():
     # print(data_list)   
     x_std, y_std = zip(*data_list)
     ax2.plot(x_std, y_std, color='black', label='$\sigma$ distribution')
-    ax2.legend(loc="upper left")
+    ax2.vlines(x=0.05, ymin=0, ymax=20340, color='gray', linestyle='--')
+    ax2.hlines(y=17562, xmin=0, xmax=0.12, color='gray', linestyle='--')
+    line_1 = ax2.scatter(0.05, 17562, c='red', marker='s', label= r'$\alpha=0.05$')
 
-
+    ax2.set_xlabel('$\sigma$ scores \n (b)', fontproperties=font_prop_lable)   
+    ax2.set_xticks([0., 0.03, 0.06, 0.09, 0.12])
+    ax2.set_yticks([0, 5000, 10000, 15000, 20000])
+    # ax2.set_ylim(0, 20380)
+    for label in (ax2.get_xticklabels() + ax2.get_yticklabels()):
+        label.set_fontproperties(font_prop)
+    
+    ax2.legend(handles=[line_1,], loc="lower right", prop=font_prop_legend) 
+    ax2.grid(which='major', linestyle='--')
 
     max_list = df['max_score'].to_numpy()
 
     error_list = np.arange(-0.7, 0.21, 0.01)
     # print(error_list)
 
-    data_max = {}
-    
+    data_max = {}    
     for i in error_list:
         count = 0
         for value in max_list:
             if value < i:
                 count += 1
         data_max[i] = count
-
-
     max_lst = sorted(data_max.items(), key=lambda x: x[0], reverse=True)
     # print(np.max(max_lst), np.min(max_lst))
     # print(data_list)   
     x_max, y_max = zip(*max_lst)
-    ax3.plot(x_max, y_max, color='black', label='$\mathit{z}_{max}$ distribution')
-    ax3.legend(loc="upper left")
+    ax3.plot(x_max, y_max, color='black', label='$\mathit{z}_{max}$ distribution')    
+   
+    ax3.vlines(x=-0.4, ymin=0, ymax=20340, color='gray', linestyle='--')
+    ax3.hlines(y=100, xmin=-0.6, xmax=0.2, color='gray', linestyle='--')
+    line_2 = ax3.scatter(-0.4, 100, c='red', marker='s', label= r"$\beta=-0.4$")
+
+    ax3.set_xlabel('$\mathit{z}_{max}$ scores \n (c)', fontproperties=font_prop_lable)   
+    ax3.set_xticks([-0.6, -0.4, -0.2, 0., 0.2])
+    ax3.set_yticks([0, 5000, 10000, 15000, 20000])
+    
+    for label in (ax3.get_xticklabels() + ax3.get_yticklabels()):
+        label.set_fontproperties(font_prop)
+    ax3.legend(handles=[line_2,], loc="upper left", prop=font_prop_legend)   
+    ax3.grid(which='major', linestyle='--')
 
     
     plt.show()
-   
+    # fig.savefig(r'iciap_data\\kf.png', format='png', dpi=600)
 
 def main():
+
+    # cnn_plot()
+
+
     criterion_plot()
 
-    print()
+   
 
 if __name__ == '__main__':
     main()
